b414c696eaa82f8295f39e13154a241bd35ed0ad
Lincoln Wu <73685301+lincolnwu@users.noreply.github.com>
2024-05-29 15:45:13 -0400

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -10,12 +10,16 @@ class InvalidKindError(Exception):
     pass
 
 
-def get_random_ingredients(kind=None):
+def get_random_ingredients(kind=None, bad=None, sus=None):
     """
     Return a list of random ingredients as strings.
 
     :param kind: Optional "kind" of ingredients.
+    :param bad: Optional "bad" ingredients.
+    :param sus: Optional "sus" ingredients.
     :type kind: list[str] or None
+    :type bad: list[pd.DataFrame] or None
+    :type sus: list[int] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
     :return: The ingredients list.
     :rtype: list[str]
